88f2f295e3655bfff4abc8933b40d1b90575d779
Sdm <100193573+Sm0810@users.noreply.github.com>
2024-07-27 00:01:01 +0530

    Add files via upload

---
+++ src/routes/+layout.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { tools } from './tools.js';
+
+// @ts-ignore
+export async function load({route, url}) {
+    return {
+        tools: tools,
+        meta: getMeta(route, url)
+    };
+}
+
+// @ts-ignore
+function getMeta(route, url) {
+    if (route.id && route.id.includes("(tools)")) {
+        // @ts-ignore
+        let tool = tools[url.pathname.replace("/","")];
+        return {
+            title: tool.name,
+            description: tool.description
+        }
+    }
+    return 0;
+}
